323acdadbdc4d59d18799694380b96f74aaec934
danieljdangelo <danieljdangelo97@gmail.com>
2023-02-05 15:10:44 -0400

    Formulario y redes listas

--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -25,4 +23,12 @@ function showSlides(n) {
   }
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+}
+
+function alerta(){
+    window.alert("Su solicitud ha sido enviada.")
+}
+
+function noSection(){
+    window.alert("No se logró hacer este apartado.")
 }
